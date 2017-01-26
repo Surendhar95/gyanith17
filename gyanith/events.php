7a751d50567a8dc7af1184e2d457b7eaeae840ad
surendhar95 <esurendhar95@gmail.com>
2017-01-26 21:06:28 +0530

    events

---
+++ gyanith/events.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+  		<title>Gyanith '17  | Feb 24-26</title>
+  		<link rel="stylesheet" href="css/style.css">
+  		<link rel="icon" type="image/png" href="images/favicon.png" sizes="32x32">
+  		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+	</head>
+	<body>
+		<header>
+			<div class="title row">
+				EVENTS
+			</div>
+		</header>
+		<div class="logo row">
+		</div>
+		<div class="dept-list">
+			<div class="list-1 row">
+				<div class="col-sm-6 col-md-6 col-lg-6">
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6">
+				</div>
+			</div>
+			<div class="list-2 row">
+				<div class="col-sm-6 col-md-6 col-lg-6">
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6">
+				</div>
+			</div>
+		</div>
+		<!-- <?php 
+			include "footer.php"
+		?> -->
+	</body>
+</html>
--- gyanith/sidebar.php
+++ gyanith/sidebar.php
@@ -1,15 +1,15 @@
 
-			<div class="menu-wrap">
+			<div class="menu-wrap" style="z-index: 7000;display: block;">
 				<nav class="menu">
-					<div class="icon-list">
-						<a href="#"><i class="fa fa-cubes" aria-hidden="true"></i><span>Events</span></a>
-						<a href="#"><i class="fa fa-cogs" aria-hidden="true"></i><span>Workshops</span></a>
-						<a href="#"><i class="fa fa-podcast" aria-hidden="true"></i><span>Guest Lectures
+					<div class="icon-list" style="color:white">
+						<a href="events.php" style="color:white"><i class="fa fa-cubes" aria-hidden="true"></i><span>Events</span></a>
+						<a href="#" style="color:white"><i class="fa fa-cogs" aria-hidden="true"></i><span>Workshops</span></a>
+						<a href="#" style="color:white"><i class="fa fa-podcast" aria-hidden="true"></i><span>Guest Lectures
 						</span></a>
-						<a href="#"><i class="fa fa-gamepad" aria-hidden="true"></i><span>Non Technical</span></a>
-						<a href="#"><i class="fa fa-bed" aria-hidden="true"></i><span>Hospitality</span></a>
-						<a href="#"><i class="fa fa-users" aria-hidden="true"></i><span>Teams</span></a>
-						<a href="#"><i class="fa fa-info-circle" aria-hidden="true"></i><span>About Us</span></a>
+						<a href="#" style="color:white"><i class="fa fa-gamepad" aria-hidden="true"></i><span>Non Technical</span></a>
+						<a href="#" style="color:white"><i class="fa fa-bed" aria-hidden="true"></i><span>Hospitality</span></a>
+						<a href="#" style="color:white"><i class="fa fa-users" aria-hidden="true"></i><span>Teams</span></a>
+						<a href="#" style="color:white"><i class="fa fa-info-circle" aria-hidden="true"></i><span>About Us</span></a>
 					</div>
 				</nav>
 				<button class="close-button" id="close-button">Close Menu</button>
