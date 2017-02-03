9d038464e3e181b843d9ab59b9396b94f86e603b
Abhiramianbu <vmkaabhirami@gmail.com>
2017-02-03 20:40:14 +0530

    project expo

---
+++ expo/content/desc.php
@@ -0,0 +1,9 @@
+<head>
+	<link href="../../style.css"/>
+</head>
+<p>                                             <center><b> "Showcase your excellence"</b></center><br/><br/>
+   It does not take much strength to do things, but it requires a great deal of strength to decide what to do. 
+   It’s time to show your creative mind in making new projects and showcasing your innovative ideas in the project 
+   expo conducted by the “INNOVATION CLUB OF NITPY “at GYANITH’ 17, the first collaborated technical fest of NITPY.
+   We welcome all the school and engineering students to present their ideas and their projects through posters or slides or working models.
+</p>   
