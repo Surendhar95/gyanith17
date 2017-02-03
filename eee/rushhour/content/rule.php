5882849b2112c2c9876ed84b98fe398de7fef391
Abhiramianbu <vmkaabhirami@gmail.com>
2017-02-03 23:27:47 +0530

    smrutis changes

--- eee/rushhour/content/rule.php
+++ eee/rushhour/content/rule.php
@@ -1,34 +1,48 @@
 <head>
 	<link href="../../style.css"/>
 </head>
-<p>1. The bots need to be programed for 1st round and shouldn’t be remote controlled. Each robot may have only the following sensors, in these maximum quantities:<br/>
-•	5 Infrared (IR) LED assemblies (#350-00017)<br/>
-•	5 Infrared Receivers (#350-00014)<br/>
-•	4 QTI Sensors (#555-27401)<br/>
+<p><ol><li> The bots need to be programed for 1st round and shouldn’t be remote controlled. 
+Each robot may have only the following sensors, in these maximum quantities:<ul>
+<li>	5 Infrared (IR) LED assemblies (#350-00017)
+</li><br><li>	5 Infrared Receivers (#350-00014)
+</li><br><li>4 QTI Sensors (#555-27401)</li>
+</ul>
+</li><br/><li>  The mass of a robot must be 500 grams or less including all attachments, parts, 
+and batteries (No spring systems shall be used at the face of the bot). The robot must 
+have a label with the team name on it during the competition.
 
-2.  The mass of a robot must be 500 grams or less including all attachments, parts, and batteries (No spring systems shall be used at the face of the bot). The robot must have a label with the team name on it during the competition.</p>br> 
+</li><br/><li> No additional power supplies are allowed. Only 4 AA 5V batteries may be used.
+ No extra batteries will be provided. No motors other than two Parallax Continuous Rotation Servos 
+ included with the SumoBot  are allowed. It is permissible to glue the rubber band tires in the SumoBot kits to 
+ the SumoBot wheels. No other tires are allowed, and nothing may be applied to the outside of the tires.
 
-3. No additional power supplies are allowed. Only 4 AA 5V batteries may be used. No extra batteries will be provided. No motors other than two Parallax Continuous Rotation Servos included with the SumoBot  are allowed. It is permissible to glue the rubber band tires in the SumoBot kits to the SumoBot wheels. No other tires are allowed, and nothing may be applied to the outside of the tires.<br/>
+</li><br/><li> Jamming devices, such as an IR LED intended to saturate the opponent's IR sensor
+ - such devices are not permitted to be operated by robots, by team members, subject
+ to disqualification, by audience members in the Competition Hall, subject to removal from the Competition Hall.
 
-4. Jamming devices, such as an IR LED intended to saturate the opponent's IR sensor - such devices are not permitted to be operated by robots, by team members, subject to disqualification, by audience members in the Competition Hall, subject to removal from the Competition Hall.</br>
+</li><br/><li> Parts that could break or damage the Sumo Ring. Do not use parts that are intended 
+to damage the opponent's robot or its operator. Normal pushes and bangs are not considered intent to damage.
 
-5. Parts that could break or damage the Sumo Ring. Do not use parts that are intended to damage the opponent's robot or its operator. Normal pushes and bangs are not considered intent to damage.</br>
+</li><br/><li> Devices that can store solids, liquid, powder, or air, or produce flame, to throw 
+at the opponent or the Sumo Ring.
 
-6. Devices that can store solids, liquid, powder, or air, or produce flame, to throw at the opponent or the Sumo Ring.<br/>
+</li><br/><li> Devices or materials to stick a robot down onto the Sumo Ring(such as double sticky 
+foam tape) or any device to assist in adding more down force (such as a vacuum device).
 
-7. Devices or materials to stick a robot down onto the Sumo Ring(such as double sticky foam tape) or any device to assist in adding more down force (such as a vacuum device).<br/>
+</li><br/><li>  Each match is estimated to be of 5 min unless extended by the event coordinators.
 
-8.  Each match is estimated to be of 5 min unless extended by the event coordinators.</br>
+</li><br/><li> When the judge or referee announces the start of the match, each contestant should activate the robot through
+ their remote control.  THE ROBOT IS NOT ALLOWED TO MOVE UNTIL 5 SECONDS AFTER ACTIVATION, during which time the players must clear out of the Ring Area.
 
-9.  When the judge or referee announces the start of the match, each contestant should activate the robot through their remote control.  THE ROBOT IS NOT ALLOWED TO MOVE UNTIL 5 SECONDS AFTER ACTIVATION, during which time the players must clear out of the Ring Area.<br/>
+</li><br/><li> Each contestant scores points based on the moves and attacks as the opponent’s bots cross the specified scoring lines in the arena (ring).
 
-10. Each contestant scores points based on the moves and attacks as the opponent’s bots cross the specified scoring lines in the arena (ring).<br/>
-
-11. When a robot is knocked over or falls over but remains within the Sumo Ring, no point is given and the round continues.<br/>
-12. A match ends when:<br/>
- a.    Either robot touches the space outside the Sumo Ring, in the Ring Area.<br/>
- b.    The end of the 5 minute overall time limit is reached without either robot exiting the Sumo Ring.<br/>
- c.    A player re-enters the Ring Area without the judge or referee's permission.<br/>
- d.    A robot does NOT remain still for at least 10 seconds after activation at the beginning of the round.<br/><br/>
-
+</li><br/><li> When a robot is knocked over or falls over but remains within the Sumo Ring, no point is given and the round continues.
+</li><br/><li> A match ends when:<ol type="a">
+ <li>    Either robot touches the space outside the Sumo Ring, in the Ring Area.
+</li><br/><li>    The end of the 5 minute overall time limit is reached without either robot exiting the Sumo Ring.
+</li><br/><li>   A player re-enters the Ring Area without the judge or referee's permission.
+</li><br/><li>    A robot does NOT remain still for at least 10 seconds after activation at the beginning of the round.
+ </li></ol>
+ </li>
+</ol>
 </p>
