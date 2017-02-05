07090935741e27006f0d2b02b08a4d8954429678
surendhar95 <esurendhar95@gmail.com>
2017-02-05 23:28:57 +0530

    clearing feb04

---
+++ expo/content/judge.php
@@ -0,0 +1,51 @@
+
+<head>
+	<link href="../../style.css"/>
+</head>
+<style>
+table, th, td {
+    border: 1px solid white;
+   
+}
+</style>
+
+<p>Evaluation will be done as follows.<br>
+	<table style="width:100%;text-align:center;">
+		<tr><th>S.No</th><th>Level</th><th>Number of questions</th><th>Marks for each question</th><th>Total marks</th><th>Time duration(in mins)</th></tr>
+		<tr>
+			<td>1.</td>
+			<td>easy</td>
+			<td>5</td>
+			<td>3</td>
+			<td>15</td>
+			<td>20</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Medium</td>
+			<td>5</td>
+			<td>5</td>
+			<td>25</td>
+			<td>30</td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>Hard</td>
+			<td>5</td>
+			<td>10</td>
+			<td>50</td>
+			<td>40</td>
+		</tr>
+		<tr>
+			<td colspan="4">Total</td>
+			
+			<td>90</td>
+			<td>90</td>
+		</tr>
+	</table>
+
+
+<br/><br/>
+
+The person who scores maximum will be the winner. The next high scorer will be runner-up.
+</p>
