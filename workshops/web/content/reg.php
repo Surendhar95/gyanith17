258823ba7428ab9e17fa392035464df434850cc4
surendhar95 <surendhar.shah96@gmail.com>
2017-02-08 23:43:54 +0530

    all fucking jobs

--- workshops/web/content/reg.php
+++ workshops/web/content/reg.php
@@ -2,9 +2,29 @@
 	<link href="../../style.css"/>
 </head>
 <p>
-<!--Click <a href="<?php /* echo "../../pdf/workshops/5.Get Plugdin - Website development using Jquery.pdf"; */ ?>" target="_blank">here</a> for payment details <br/>-->
-Payment details will be updated on February 8 at 10 AM. <br/>
-Click <a href="<?php echo "../../dashboard.php"; ?>">here</a> to register for Workshops
- <br/>
 
+
+Click <a href="<?php echo "../../dashboard.php"; ?>">here</a> to register for Workshop.
+ <br/><br>
+You should register for workshops only after payment is made. No registrations will be confirmed if payment details are not provided. If you are unable to pay the full amount now, you can pay 50% of the total amount and make remaining payment one day before the date of workshop. In case you fail to make the remaining payment, your registration will be canceled and no reimbursement will be given.
+There are three options for participants to pay the amount.<br><br>
+1. Internet banking: If you have internet banking, you can transfer the amount to following account<br><br>
+Account name: GYANITH<br>
+Account no: 36521315650<br>
+IFSC: SBIN0001418<br>
+Branch: SBI Karaikal<br>
+Once the payment is made, send the receipt to workshops@gyanith.org along with your team name, team members,their GY-ID (Gyanith ID) and name of workshop(s) you want to participate. <br><br>
+
+2. Transfer through NEFT <br><br>
+Account name: GYANITH<br>
+Account no: 36521315650<br>
+IFSC: SBIN0001418<br>
+Branch: SBI Karaikal<br>
+Once the payment is made, send the UTR number to workshops@gyanith.org along with your team name, team <br>members, their GY-ID (Gyanith ID) and name of workshop(s) you want to participate. <br>
+<br><br>
+3. Through Demand Draft: <br><br>
+You can take a DD for the amount in favor of GYANITH NIT PUDUCHERRY payable at SBI Karaikal. You have to send a scanned copy of the DD and send to workshops@gyanith.org along with your team name, team members, their GY-ID (Gyanith ID) and name of workshop(s) you want to participate. 
+<br><br>
+4.SBI Collect<br><br>
+Click <a href="<?php echo "../../pdf/workshops/5.Get Plugdin - Website development using Jquery.pdf"; ?>" target="_blank">here</a> to refer how to pay through SBI Collect<br/>
 </p>
