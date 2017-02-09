4f49de4e29db6a767522c3e0c18b6dff11b5fbc3
surendhar95 <surendhar.shah96@gmail.com>
2017-02-09 15:06:47 +0530

    workshop payment details

--- workshops/swarm/content/reg.php
+++ workshops/swarm/content/reg.php
@@ -1,3 +1,4 @@
+
 <head>
 	<link href="../../style.css"/>
 </head>
@@ -5,8 +6,19 @@
 
 
 Click <a href="<?php echo "../../dashboard.php"; ?>">here</a> to register for Workshop.
- <br/><br>
-You should register for workshops only after payment is made. No registrations will be confirmed if payment details are not provided. If you are unable to pay the full amount now, you can pay 50% of the total amount and make remaining payment one day before the date of workshop. In case you fail to make the remaining payment, your registration will be canceled and no reimbursement will be given.
+<br/><br>
+You should register for workshops only after payment is made. No registrations will be confirmed if payment details are not provided.<br>
+
+In dashboard after the payment has been done provide the Reference number of your transaction(SBI Collect Reference number/UTR Number/DD number/Net banking transaction number)<br><br>
+
+1.SBI Collect<br><br>
+Note:Only full payment is accepted through SBI Collect<br>
+In SBI Collect Debit cards,Credit cards,Netbanking and other payment modes are accepted.<br>
+Click <a href="<?php echo "../../pdf/workshops/3.Take Control - Swarm Robotics.pdf"; ?>" target="_blank">here</a> to refer how to pay through SBI Collect<br/>
+Link : <a href="<?php echo "https://www.onlinesbi.com/prelogin/icollecthome.htm"; ?>">SBI Collect</a><br><br>
+
+ 
+If you are unable to pay the full amount now, you can pay 50% of the total amount by any of the following methods and make remaining payment one day before the date of workshop. In case you fail to make the remaining payment, your registration will be canceled and no reimbursement will be given.
 There are three options for participants to pay the amount.<br><br>
 1. Internet banking: If you have internet banking, you can transfer the amount to following account<br><br>
 Account name: GYANITH<br>
@@ -25,6 +37,5 @@ Once the payment is made, send the UTR number to workshops@gyanith.org along wit
 3. Through Demand Draft: <br><br>
 You can take a DD for the amount in favor of GYANITH NIT PUDUCHERRY payable at SBI Karaikal. You have to send a scanned copy of the DD and send to workshops@gyanith.org along with your team name, team members, their GY-ID (Gyanith ID) and name of workshop(s) you want to participate. 
 <br><br>
-4.SBI Collect<br><br>
-Click <a href="<?php echo "../../pdf/workshops/3.Take Control - Swarm Robotics.pdf"; ?>" target="_blank">here</a> to refer how to pay through SBI Collect<br/>
+
 </p>
