12150e3b2c3c44d408e8286cffcf955a05ff78e5
surendhar95 <surendhar.shah96@gmail.com>
2017-02-10 16:49:48 +0530

    team page

---
+++ team.php
@@ -0,0 +1,380 @@
+<html>
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<title>Gyanith '17  | Teams</title>
+  
+  		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css"/>
+  		<!--<link rel="stylesheet" type="text/css" href="css/menu_sideslide.css" />-->
+  		<link rel="icon" type="image/png" href="images/favicon.png" sizes="32x32">
+
+  		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+  		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/menu_sideslide.css" />
+		<link rel="stylesheet" href="css/material.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style_click_drop.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+  		<script type="text/javascript" src="js/snap.svg-min.js"></script>
+  		<script type="text/javascript" src="js/events.js"></script>
+		<script src="js/material.min.js"></script>
+		<script src="https://use.fontawesome.com/ce3b4b3f41.js"></script>
+
+
+	</head>
+  <style>
+	.thumbnail {
+	background:rgba(255, 255, 255, 0.75);
+    position:relative;
+    overflow:hidden;
+	-webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+}
+ 
+.caption {
+    
+    background:rgba(25, 246, 232, 0);
+
+    text-align:center;
+    color:#000000;
+    z-index:2;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+}
+.thumbnail:hover {
+	background:rgba(25, 246, 232, 0.50);
+	color: #fff;
+    
+}
+.container-fluid {
+	position: relative;
+	top:-15%;
+}
+  </style>
+  <body class="event-gradient" style="overflow-y: auto;">
+  <?php
+    include 'loginicon.php';
+    include("sidebar.php");
+   ?>
+    <header>
+      <div class="title row">
+        <h2><b>TEAM</b></h2>
+      </div>
+    </header>
+<div class="container-fluid">
+<div class="row-fluid" style="width:90%; padding-left: 150px; padding-top: 150px;">
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/1.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">BALA MURUGAN<br>PRESIDENT<br>+91-9444616846</p>
+					</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/2.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">THIPPARTHI PRASAD
+					<br>SECRETARY
+					<br>+91-9489453595</p>
+					</div>
+			</div>
+		</div>
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/3.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">JAKKULA AKHIL
+						<br>OVERALL COORDINATOR
+						<br>+91-9944697005</p>
+						</div>
+				</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="team/4.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">MARISSA CATHERINE N
+						<br>OVERALL COORDINATOR
+						<br>+91-9679533132</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class=container-fluid>
+	<div class="row-fluid" style="width:90%; padding-left: 150px;">
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/5.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">MACHARLA NIKHIL
+						<br>VICE PRESIDENT
+						<br>+91-7680909060</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/6.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">YELURI GIFT BABU
+						<br>JOINT SECRETARY
+						<br>+91-9642668526</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/7.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">SURENDHAR E
+						<br>HEAD WEB OPERATIONS
+						<br>+91-8489821694</p>
+						</div>
+				</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="team/8.jpg" alt="Team1" style="width:100%">
+				
+					<div class="caption">
+						<p class="text-center">SURENDAR S
+						<br>HEAD ONLINE ACTIVITIES
+						<br>+91-9092671624</p>
+						</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+<div class="row-fluid" style="width:90%; padding-left: 150px; ">
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/9.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">CHANTHIRAPRABU B S
+						<br>HEAD WORKSHOPS
+						<br>+91-9486850501</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/10.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">VANI LAKSHMI POOJITHA DESU
+						<br>HEAD EVENTS
+						<br>+91-8508815394</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/11.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">BRAHMA SANTOSH
+						<br>HEAD EVENTS
+						<br>+91-8500519192</p>
+						</div>
+				</div>
+		</div>
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/12.png" alt="Team1" style="height:33%;width:100%">
+					<div class="caption">
+					<p class="text-center">MADASU UTTAM KUMAR
+						<br>HEAD EVENTS
+						<br>+91-9445984273</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class=container-fluid>
+	<div class="row-fluid" style="width:90%; padding-left: 150px;">
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/13.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">SWARAJ MONDAL
+						<br>HEAD EVENTS
+						<br>+91-9476051453</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/22.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">CHRISTINE GEORGE
+						<br>HEAD CONTENTS
+						<br>+91-8508814959</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="team/20.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">K SRIKANTH
+						<br>HEAD FINANCE
+						<br>+91-8883852707</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/15.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">SRIDHAR L
+						<br>HEAD PUBLICITY
+						<br>+91-9578438591</p>
+						</div>
+				</div>
+		</div>
+	
+		
+	</div>
+</div>
+<div class=container-fluid>
+	<div class="row-fluid" style="width:90%; padding-left: 150px;">
+		<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/14.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">GIRIDHARAN S
+						<br>HEAD PUBLICITY
+						<br>+91-8903426634</p>
+					
+				</div>
+			</div>
+		</div>
+	<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="team/16.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">RAMKUMAR A R
+						<br>HEAD MARKETING
+						<br>+91-9659293141</p>
+					
+				</div>
+			</div>
+		</div>
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/17.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">K.G.ANANDARAMAN
+						<br>HEAD MARKETING
+						<br>+91-9715540595</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="thumbnail"> 
+			<img src="team/18.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">AKSHAY M. M
+						<br>HEAD HOSPITALITY
+						<br>+91-7598181886</p>
+					
+				</div>
+			</div>
+		</div>
+		
+		
+	</div>
+</div>
+<div class=container-fluid>
+	<div class="row-fluid" style="width:90%; padding-left: 150px;">
+	<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/19.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">G.PRAVIN KUMAR
+						<br>HEAD HOSPITALITY
+						<br>+91-9489736577</p>
+						</div>
+				</div>
+		</div>
+
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/21.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">AASHISH K MOORTHY
+						<br>HEAD PROGRAMMING
+						<br>+91-9744036975</p>
+					
+				</div>
+			</div>
+		</div>
+
+		
+		<div class="col-sm-3 ">
+				<div class="thumbnail">
+				<img src="team/23.jpg" alt="Team1" style="width:100%">
+						<div class="caption">
+						<p class="text-center">AMMU BINDU
+						<br>HEAD NON-TECHNICAL
+						<br>+91-8508825316</p>
+						</div>
+				</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="team/24.jpeg" alt="Team1" style="height:33%;width:100%">
+				<div class="caption">
+					<p class="text-center">RAQEEB MOHAMED
+						<br>HEAD NON-TECHNICAL
+						<br>+91-9047375357</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class=container-fluid>
+	<div class="row-fluid" style="width:90%; padding-left: 150px;">
+	<div class="col-sm-3">
+		<div class="thumbnail">
+			<img src="team/25.jpg" alt="Team1" style="width:100%">
+				<div class="caption">
+					<p class="text-center">DARSHANAM RAHUL
+						<br>HEAD NON-TECHNICAL
+						<br>+91-8220522059</p>
+					
+				</div>
+			</div>
+		</div>
+</div>
+ <?php 
+			include ('footer.php');
+		?> 
+		<script  src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+			<script src="js/classie.js"></script>
+		<script src="js/main.js"></script>
+</body>
+	</html>
