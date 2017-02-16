0d5caa53366731ed649ddfc9134773102fc58218
surendhar95 <surendhar.shah96@gmail.com>
2017-02-16 18:20:25 +0530

    cobweb new

--- nontech/cobweb/content/format.php
+++ nontech/cobweb/content/format.php
@@ -1,13 +1,15 @@
 <head>
 	<link href="../../style.css"/>
 </head>
-<p><ol><li>The questions will be available on the portal on 13th of March 2016 at 12:00 PM.
+<p><ol><li>The questions will be available on the portal on 17th of March 2016 at 12:00 PM.
+<li>You need to register for the event in your dashboard.Once you are registered for the event,the link to the game portal will be available for the registered users only,in main page of the website.
 
+</li><br>
 </li><br><li>Questions comprises of a picture and sometimes a hint.
 
 </li><br><li>As you solve each question you will get access to next one or two questions which will lead you to the final question.
 
-</li><br><li>The event closes on or before 18th of March 2016.
+</li><br><li>The event closes on or before 22th of March 2016 at 12:00 PM.
 
 </li><br><li>In case of a tie ,then whoever solved them first is given priority.
 </li>
