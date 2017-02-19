b4dab886a05a8c4abc54dfad58eb63152fe88a8e
Abhiramianbu <vmkaabhirami@gmail.com>
2017-02-19 11:47:06 +0530

    sync format change

--- eee/sync/content/desc.php
+++ eee/sync/content/desc.php
@@ -1,18 +1,10 @@
 <head>
 	<link href="../../style.css"/>
 </head>
-<p>If you are a person who can handle a combination of modeling, design techniques and analyses of electrical and electronic circuits, then search no more, this is the event for you. Come and explore your talent with hands on experience in simulation and circuit construction. It also covers concepts on Basic electrical models such as machines and circuits on power electronics in MATLAB. Come participate and bring out the ultimate engineer in you.  
-
-
-<!--
-<br>
-<p id="prizepara">
-Price Money<br/>
-First Price : <span id="rupee">6000 </span>(INR) &nbsp;&nbsp;&nbsp;
-Second Price: <span id="rupee">3000 </span>(INR)
-</p>
+<p><!--If you are a person who can handle a combination of modeling, design techniques and analyses of electrical and electronic circuits, then search no more, this is the event for you. Come and explore your talent with hands on experience in simulation and circuit construction. It also covers concepts on Basic electrical models such as machines and circuits on power electronics in MATLAB. Come participate and bring out the ultimate engineer in you.  
 -->
 
+It involves hands on experience on two areas – Simulation and Circuit Construction. This includes practical exposure to modelling, design and analysis of electrical and electronic circuits .
 
 <!--prize start-->
 <br/>
