7cfca9dc269f2f421d2b869044e29c1d189379e9
Abhiramianbu <vmkaabhirami@gmail.com>
2017-02-19 23:58:01 +0530

    sponsor

---
+++ sponsors.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html>
+<head>
+	<title>Gyanith '17  | SPONSORS</title>
+	<link rel="icon" type="image/png" href="images/favicon.png" sizes="32x32">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+  	<link rel="stylesheet" href="css/style.css">
+  	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+  	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/menu_sideslide.css" />
+	<link rel="stylesheet" href="css/material.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style_click_drop.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/material.min.js"></script>
+	<script src="https://use.fontawesome.com/ce3b4b3f41.js"></script>
+
+<style type="text/css">	
+	p,h1,a{
+		font-family: 'Droid Sans', sans-serif;
+
+	}
+html {  
+    overflow-y:auto;
+    background: #a9e4f7; /* Old browsers */
+    background-color:#222222;
+    filter:progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr=#222222, endColorstr=#00ACE9);
+    background-image:-moz-linear-gradient(top, #222222 0%, #00ACE9 50%,#222222 100%);
+    background-image:linear-gradient(top, #222222 0%, #00ACE9 50%,#222222 100%);
+    background-image:-webkit-linear-gradient(top, #222222 0%, #00ACE9 50%,#222222 100%);
+    background-image:-o-linear-gradient(top, #222222 0%, #00ACE9 50%,#222222 100%);
+    background-image:-ms-linear-gradient(top, #222222 0%, #00ACE9 50%,#222222 100%);
+    background-attachment: fixed;
+}
+
+div{
+	background:transparent;
+}
+body {
+	color:#000;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  background:transparent;
+}
+
+
+.row{
+	
+	background:transparent;
+	background-color:transparent;
+}
+.row h4{
+    text-align:center
+}
+header{
+	position:relative;
+	top:2%;
+	left:41%;
+}
+.spon-row{
+    
+    width:95%;
+    margin:auto;
+   /* border: 5px solid #fff;*/
+}
+.spon-col{
+    height:40%;
+    width:40%;
+}
+</style>
+
+</head>
+<body >
+    <div class="row" style="height:90%">
+    <?php
+	    include ('loginicon.php');
+		include("sidebar.php");
+	 ?>
+     
+	     <header>
+			<div class="title1 row">
+				<h2><b>SPONSORS</b></h2>
+			</div>
+		</header>
+        <br/><br/><br/><br/><br/>
+        <div class="container-fluid" style="overflow-y:auto;">        
+            <div class="row">
+                <h4>TITLE SPONSOR</h4> 
+                <div class="row spon-row">
+                    <div class="col-md-5"></div>
+                    <div class="col-md-5" style="position:relative;left:2%;">
+                        <div class="spon-col">
+                            <img src="sponsor/ongc.jpg" style="height:70%;width:70%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <br/><br/><br/>
+            <div class="row">
+                <h4>MEDIA PARTNERS</h4>
+                <div class="row spon-row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <div class="spon-col" >
+                            <img src="sponsor/diamond.jpg" style="height:90%;width:90%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="position:relative;left:-15%;top:30px;">
+                        <div class="spon-col" >
+                            <img src="sponsor/suriyanfm.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/><br/><br/>
+            <div class="row">
+                <h4>COUPON SPONSORS</h4>
+                <div class="row spon-row" style="padding-left:20%;">
+                    <div class="col-md-4">
+                        <div class="spon-col" >
+                            <img src="sponsor/kfc.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="spon-col" >
+                            <img src="sponsor/dominos.jpg" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="position:relative;top:50px;">
+                        <div class="spon-col">
+                            <img src="sponsor/green.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                </div>
+                <br/><br/>
+                <div class="row spon-row" style="padding-left:20%;">
+                    <div class="col-md-4">
+                        <div class="spon-col" style="position:relative;top:45px;" >
+                            <img src="sponsor/makemy.jpg" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="spon-col" >
+                            <img src="sponsor/pizza.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4" >
+                        <div class="spon-col">
+                            <img src="sponsor/marry.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/><br/><br/>
+            <div class="row">
+                <h4>OTHER SPONSORS</h4>
+                <div class="row spon-row" style="padding-left:20%;">
+                    <div class="col-md-4">
+                        <div class="spon-col"  >
+                            <img src="sponsor/ace.png" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="spon-col" style="position:relative;top:45px;">
+                            <img src="sponsor/alpha.jpg" style="height:150%;width:150%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="position:relative;top:35px;">
+                        <div class="spon-col">
+                            <img src="sponsor/everbrightcem.PNG" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div class="row spon-row" style="padding-left:20%;">
+                    <div class="col-md-4">
+                        <div class="spon-col"  >
+                            <img src="sponsor/ramanas.PNG" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="spon-col" style="position:relative;top:45px;">
+                            <img src="sponsor/kumar.jpg" style="height:150%;width:150%;">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="spon-col">
+                            <img src="sponsor/necklace.jpg" style="height:100%;width:100%;">
+                        </div>
+                    </div>
+                </div>
+             </div>    
+            
+        </div>
+    </div>
+      <br/><br/><br/>
+      
+
+<div class="row" style="height:10%">
+        <?php 
+			include ('footer.php');
+		?> 
+     </div>
+		<script  src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+			<script src="js/classie.js"></script>
+		<script src="js/main.js"></script>
+</body>
+</html>
